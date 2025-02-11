f1582446dc5a5e1e8197b5f44970ed7428d546b9
Sannidhi-17 <sannidhibookseller@gmail.com>
2025-02-11 01:01:34 -0700

    Hyperparameter Added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,11 +46,45 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor()
             }
 
-            model_report:dict = evaluate_model(X_train = X_train, 
+            params = {
+                "Decision Tree": {
+                    "criterion": ["squared_error"]
+                },
+                "Random Forest": {
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [ .1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8,16,32,64, 128, 256]
+                },
+                "Linear regression": {
+                    # "fit_intercept": [True, False]
+                },
+                "K-NN":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGBOOST":{
+                    'learning_rate': [ .1, .01, .05, .001],
+                    'n_estimators': [8,16,32,64, 128, 256]
+                },
+                "catBoosting Classifier":{
+                    'depth': [ 6,9,10],
+                    'learning_rate': [ .1, .01, .05, .001]
+                },
+                "AdaBoost Classifier":{
+                    'learning_rate': [ .1, .01, .05, .001],
+                    'n_estimators': [8,16,32,64, 128, 256]
+                }
+            }
+
+            model_report, trained_models = evaluate_model(X_train = X_train, 
                                             y_train=y_train,
                                             X_test=X_test, 
                                             y_test=y_test, 
-                                            models = models)
+                                            models = models,
+                                            param=params
+                                            )
 
             
             ## To get best model score from dict
@@ -61,8 +95,11 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
-            best_model = models[best_model_name]
+            print("x-x-x-")
+            print(best_model_name)
+            best_model = trained_models[best_model_name]
+            logging.info(f"best_model: {best_model}")
+            
             if best_model_score < 0.6:
                 logging.info("No Best Model Is Found")
                 raise CustomException("No best model")
